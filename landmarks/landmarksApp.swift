207e80640f323f8322f9832fc74db090563415f5
Abhishek Doshi <adoshi26.ad@gmail.com>
2024-02-23 18:19:28 +0530

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Abhishek Doshi on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
